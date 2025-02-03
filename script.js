e33854ae9a9aa3e3b1367461179b6b281dad1626
sabur-hub <saburnorov@gmail.com>
2025-02-03 08:29:39 +0300

    add new schedule

--- script.js
+++ script.js
@@ -4,68 +4,24 @@ window.addEventListener('DOMContentLoaded', function () {
     var currentDate = new Date();
     var currentDay = currentDate.getDay(); // 0 - Воскресенье, 1 - Понедельник, ..., 6 - Суббота
 
-    // Цвета для каждого дня недели
-    var colors = ['#FC926D', '#FCCE6D', '#8A6DFC', '#74FC6D', '#6D97FC', '#6DFCFA', '#ffffff']; // Белый цвет для воскресенья
-
-    // В зависимости от текущего дня недели применяем стиль к соответствующему блоку
     if (currentDay === 0) {
         restDayMessage.style.display = 'block';
     } else {
-        blocks[currentDay - 1].style.backgroundColor = colors[currentDay - 1]; // Устанавливаем цвет для текущего дня
+        var todayBlock = blocks[currentDay - 1];
+        todayBlock.classList.add('highlight');
+        
+        // Автоскролл до сегодняшнего дня
+        todayBlock.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
 
-    // Обработчики событий для кнопок и модального окна
+    // Темный режим
     var darkModeToggle = document.getElementById('darkModeToggle');
-    var authorInfoButton = document.getElementById('authorInfoButton');
-    var authorInfoModal = document.getElementById('authorInfoModal');
-    var closeButton = document.getElementsByClassName('close')[0];
-
-    // Обработчик клика для кнопки переключения в тёмный режим
     darkModeToggle.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
-        updateDarkModeButtonText(); // Обновляем текст кнопки при переключении режима
+        updateDarkModeButtonText();
     });
 
-    // Обработчик клика для кнопки об авторе
-    authorInfoButton.addEventListener('click', function () {
-        authorInfoModal.style.display = 'block';
-    });
-
-    // Обработчик клика для кнопки закрытия модального окна
-    closeButton.addEventListener('click', function () {
-        authorInfoModal.style.display = 'none';
-    });
-
-    // Закрытие модального окна при клике за его пределами
-    window.addEventListener('click', function (event) {
-        if (event.target == authorInfoModal) {
-            authorInfoModal.style.display = 'none';
-        }
-    });
-
-    // Устанавливаем начальный текст кнопки
     function updateDarkModeButtonText() {
-        var isDarkMode = document.body.classList.contains('dark-mode');
-        darkModeToggle.textContent = isDarkMode ? 'Ҳолати рӯшан' : 'Ҳолати торик';
+        darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? 'Ҳолати рӯшан' : 'Ҳолати торик';
     }
-});
-// service-worker.js
-
-self.addEventListener('install', function(event) {
-event.waitUntil(
-    caches.open('cache').then(function(cache) {
-    return cache.addAll([
-        '/',
-        '/meh.html',
-    ]);
-    })
-);
-});
-
-self.addEventListener('fetch', function(event) {
-event.respondWith(
-    caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-    })
-);
 });
